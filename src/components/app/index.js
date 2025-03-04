7cf77945e0a9564e2dfe0001731280bfa4bc7c4f
Nikolai Volosiankov <wol87@ya.ru>
2025-03-04 15:14:59 +0100

    feat: Add urlIngredients

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,17 +1,39 @@
 import styles from './app.module.css';
-import ingredientsData from '../../utils/data';
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
+import { useEffect, useState } from 'react';
+
+const urlIngredients = 'https://norma.nomoreparties.space/api/ingredients';
+
 const App = () => {
-	const bun = ingredientsData.find((item) => item.type === 'bun');
-	const items = ingredientsData.filter((item) => item.type !== 'bun');
+	const [ingredients, setIngredients] = useState([]);
+	useEffect(() => {
+		try {
+			const fetchIngredients = async () => {
+				const res = await fetch(urlIngredients);
+				const data = await res.json();
+				setIngredients(data.data);
+			};
+			fetchIngredients();
+		} catch (error) {
+			console.error('Ошибка при получении данных:', error);
+		}
+	}, []);
+
+	const filterBun = (ingredients) => {
+		return ingredients.find((item) => item.type === 'bun');
+	};
+
 	return (
 		<div className={styles.app}>
 			<AppHeader />
 			<main className={styles.main}>
-				<BurgerIngredients ingredients={ingredientsData} />
-				<BurgerConstructor bun={bun} items={items} />
+				<BurgerIngredients ingredients={ingredients} />
+				<BurgerConstructor
+					ingredients={ingredients}
+					bun={filterBun(ingredients)}
+				/>
 			</main>
 		</div>
 	);
