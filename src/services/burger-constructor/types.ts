b6e5e30caf13f910ad6f651cb1c175ed0a4e4c6d
Nikolai Volosiankov <wol87@ya.ru>
2025-05-07 15:06:48 +0200

    Update burger-constructor types

---
+++ src/services/burger-constructor/types.ts
@@ -0,0 +1,40 @@
+import {
+	ADD_INGREDIENT,
+	REMOVE_INGREDIENT,
+	SET_INGREDIENTS,
+	RESET_CONSTRUCTOR,
+	SET_BUN,
+} from './constants';
+import { TIngredient } from '@utils/types';
+
+export type TAddIngredient = {
+	readonly type: typeof ADD_INGREDIENT | typeof SET_BUN;
+	readonly payload: TIngredient;
+};
+
+export type TRemoveIngredient = {
+	readonly type: typeof REMOVE_INGREDIENT;
+	readonly payload: number;
+};
+
+export type TSetIngredients = {
+	readonly type: typeof SET_INGREDIENTS;
+	readonly payload: Array<TIngredient>;
+};
+
+export type TResetConstructor = {
+	readonly type: typeof RESET_CONSTRUCTOR;
+};
+
+export type TBurgerConstructorActions =
+	| TAddIngredient
+	| TRemoveIngredient
+	| TSetIngredients
+	| TResetConstructor;
+
+export type TBurgerConstructorState = {
+	ingredients: Array<TIngredient>;
+	bun: TIngredient | null;
+	isLoading: boolean;
+	error: string | null;
+};
