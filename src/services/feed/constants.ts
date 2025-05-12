fde1dea0a068e7789477c7e038da877b0a650ca6
Nikolai Volosiankov <wol87@ya.ru>
2025-05-12 11:06:55 +0200

    Delete WS_SEND_MESSAGE

--- src/services/feed/constants.ts
+++ src/services/feed/constants.ts
@@ -3,4 +3,3 @@ export const WS_CONNECTION_SUCCESS = 'WS_CONNECTION_SUCCESS' as const;
 export const WS_CONNECTION_ERROR = 'WS_CONNECTION_ERROR' as const;
 export const WS_CONNECTION_CLOSED = 'WS_CONNECTION_CLOSED' as const;
 export const WS_GET_MESSAGE = 'WS_GET_MESSAGE' as const;
-export const WS_SEND_MESSAGE = 'WS_SEND_MESSAGE' as const;
--- src/services/feed/types.ts
+++ src/services/feed/types.ts
@@ -5,7 +5,6 @@ import {
 	WS_CONNECTION_ERROR,
 	WS_CONNECTION_CLOSED,
 	WS_GET_MESSAGE,
-	WS_SEND_MESSAGE,
 } from './constants';
 
 export type TFeedWsGetMessagePayload = {
@@ -37,18 +36,12 @@ export type TFeedWsConnectionClosed = {
 	readonly type: typeof WS_CONNECTION_CLOSED;
 };
 
-export type TFeedWsSendMessage = {
-	readonly type: typeof WS_SEND_MESSAGE;
-	readonly payload: Event;
-};
-
 export type TFeedActions =
 	| TFeedWsConnectionStart
 	| TFeedWsConnectionSuccess
 	| TFeedWsConnectionError
 	| TFeedWsConnectionClosed
-	| TFeedWsGetMessage
-	| TFeedWsSendMessage;
+	| TFeedWsGetMessage;
 
 export type TFeedState = {
 	wsConnected: boolean;
